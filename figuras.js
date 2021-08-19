0edb5fec2a00164404d7d8c98da4708b22358f9c
Diego F. Vasquez P <vapdif@gmail.com>
2021-08-19 18:40:57 -0500

    main

--- figuras.js
+++ figuras.js
@@ -1,3 +1,5 @@
+// cuadrado
+
 const ladoC = 5;
 console.log("los lados del cuadrado miden: " +ladoC + "cm");
 
@@ -6,4 +8,18 @@ console.log("el perimetro del cuadrado es: " +perimetroC + "cm");
 
 const areaC = ladoC * ladoC;
 console.log("el área del cuadrado es: " +areaC + "cm²");
+
+
+// triangulo 
+const ladoT1 = 6;
+const ladoT2 = 6;
+const ladoT3 = 4;
+
+console.log("los lados del triangulo miden: " 
+    + ladoT1 + "cm " 
+    + ladoT2 + "cm " 
+    + ladoT3 + "cm "
+
+);
+
 
