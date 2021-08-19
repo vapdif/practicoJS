087b7ced46635efd783a4d11b43cc2c0d612be08
Diego F. Vasquez P <vapdif@gmail.com>
2021-08-19 18:46:37 -0500

    main

--- figuras.js
+++ figuras.js
@@ -1,5 +1,5 @@
 // cuadrado
-
+console.group("Cuadrado");
 const ladoC = 5;
 console.log("los lados del cuadrado miden: " +ladoC + "cm");
 
@@ -10,16 +10,18 @@ const areaC = ladoC * ladoC;
 console.log("el área del cuadrado es: " +areaC + "cm²");
 
 
+console.group("Triangulo");
+
 // triangulo 
 const ladoT1 = 6;
 const ladoT2 = 6;
 const ladoT3 = 4;
 
-console.log("los lados del triangulo miden: " 
+console.log("los lados del triangulo son: " 
     + ladoT1 + "cm " 
     + ladoT2 + "cm " 
     + ladoT3 + "cm "
-
 );
+
 
 
