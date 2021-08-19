b5a1248e30c8cfa3fe397235c37be04e53d749d5
Diego F. Vasquez P <vapdif@gmail.com>
2021-08-18 20:08:51 -0500

    main

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # practicoJS
+
+
+## Taller # 1: figuras geométricas
+
+
+- Primer paso: definir las fórmulas 
+- Segundo paso: implementar formulas en JS
+- Tercer paso: crear funciones
+- Cuarto paso: integral JS con HTML
+
---
+++ figuras.js
@@ -0,0 +1,9 @@
+const ladoC = 5;
+console.log("los lados del cuadrado miden: " +ladoC + "cm");
+
+const perimetroC = ladoC * 4;
+console.log("el perimetro del cuadrado es: " +perimetroC + "cm");
+
+const areaC = ladoC * ladoC;
+console.log("el área del cuadrado es: " +areaC + "cm²");
+
