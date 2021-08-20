19c90e6ce3f9e9899567f0c41541e6febef55b1d
Diego F. Vasquez P <vapdif@gmail.com>
2021-08-19 20:29:44 -0500

    main

--- figuras.js
+++ figuras.js
@@ -8,20 +8,46 @@ console.log("el perimetro del cuadrado es: " +perimetroC + "cm");
 
 const areaC = ladoC * ladoC;
 console.log("el área del cuadrado es: " +areaC + "cm²");
+console.groupEnd();
 
-
+// triangulo
 console.group("Triangulo");
-
-// triangulo 
 const ladoT1 = 6;
 const ladoT2 = 6;
 const ladoT3 = 4;
+const altT = 5.5;
+const perTri = ladoT1+ladoT2+ladoT3 
+const area = (ladoT3 * altT)/2;
 
 console.log("los lados del triangulo son: " 
     + ladoT1 + "cm " 
     + ladoT2 + "cm " 
     + ladoT3 + "cm "
 );
+console.log("la altura es: " +altT + "cm ");
+console.log("el perimetro es: " +perTri + "cm ");
+console.log("el area es: " +area + "cm² ");
+
+
+console.groupEnd();
+
+
+// circulo
+console.group("Circulo");
+
+const radio = 5;
+const diametroC = 2 * radio;
+const pi = Math.PI;
+const perimetroC = diametroC * pi;
+const areaC = (radio*radio) * pi;
+
+console.log("el radio es: " +radio + "cm");
+console.log("el diametro es: " +diametroC + "cm");
+console.log("el PI es: " + pi + "cm");
+console.log("el perimnetro es: " +perimetroC + "cm");
+console.log("el area es: " +areaC + "cm²");
+
+console.groupEnd();
 
 
 
